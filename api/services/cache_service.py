c4cb750cda39761c3298e42e05f4f524c24a6139
Sunny Kumar <sunny>
2025-03-25 14:28:59 +0530

    remove unused code from cache

--- api/services/cache_service.py
+++ api/services/cache_service.py
@@ -18,35 +18,9 @@ class CacheProcessor:
             return json.loads(cached_data)
         return False
 
-    # def get_response_from_cache(self, data):
-    #     output = []
-    #     for site in SITELIST:
-    #         key = f"{data.sourceIATA}:{data.destinationIATA}:{data.departureDate}:{data.site}"
-    #         cached_response = self.client.get(key)
-    #         if cached_response:
-    #             output.append(cached_response)
-    #         else:
-    #             print("cache miss")
-    #             # Send request to MessageProcessor
-    #             MessageProcessor().process(data)
-
-
     def set_response_to_cache(self, key, value, expiration: int =None):
         '''
         Set cache value to redis
         '''
         self.client.set(key, json.dumps(value), ex=expiration)
         return True
-
-
-
-# key = f"{data.sourceIATA}:{data.destinationIATA}:{data.departureDate}:{site}"
-# value = {"sourceIATA": data.sourceIATA, "destinationIATA": data.destinationIATA, "departureDate": data.departureDate, "siteId": site}
-# cache.set_response_to_cache(key, value, expiration=60)
-# output.append ({
-#     "details":value,
-#     "message": "Response added to cache",
-#     "siteId": site
-# })
-# cache will send the response to the client
-# Add value to the Database(mongodb)
